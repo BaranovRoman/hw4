0bb2c0c78ccefc5e776f0c25fef07f5f84de65f8
Roman Baranov <info@romanbaranov.pro>
2019-10-08 18:43:43 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <template>
+      <h1 v-if="radio">{{ msgR }}</h1>
+      <h1 v-if="check">{{ msgC }}</h1>
+      <h1 v-if="table">{{ msgT }}</h1>
+      <hr>
+      <template v-if="radio">
+        <div class="form-group" v-for="(inp, index) in answersR" :key="index">
+          <input type="radio" class="form-check-input" :value="inp" v-model="a"> {{ inp }}
+        </div>
+      </template>
+      <template v-if="check">
+        <div class="form-group" v-for="(inp, indexx) in answersR" :key="indexx">
+          <input type="checkbox" :value="inp" v-model="b"> {{ inp }}
+        </div>
+      </template>
+      <hr>
+      <button class="btn btn-success" v-if="radio" @click.prevent="radioAns">Ответить</button>
+      <button class="btn btn-success" v-if="check" @click.prevent="checkAns">Ответить</button>
+      </template>
+      <table v-if="table" class="table table-bordered">
+        <tr>
+          <td>
+            Вопрос
+          </td>
+          <td>
+            Ваш ответ
+          </td>
+        </tr>
+        <tr>
+          <td>
+            {{ msgR }}
+          </td>
+          <td>
+            {{ a }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            {{ msgC }}
+          </td>
+          <td>
+            {{ b }}
+          </td>
+        </tr>
+      </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import List from './components/list'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      msgR: 'Какой тег задаёт ссылку?',
+      msgC: 'Какие теги являются строчными',
+      msgT: 'Итого',
+      radio: true,
+      check: false,
+      table: false,
+      answersR: ['div', 'span', 'a', 'table'],
+      answersC: ['div', 'span', 'a', 'table'],
+      a: '',
+      b: []
+    }
+  },
+  methods: {
+    radioAns() {
+      this.radio = false;
+      this.check = true;
+    },
+    checkAns() {
+      this.check = false;
+      this.table = true;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/list.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="form-group" v-for="(inp, index) in answers" :key="index">
+        <input type="checkbox" :value="inp" v-model="b"> {{ inp }}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            answers: ['div', 'a', 'span', 'table']
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
